507c9f27c7491fcb9346796e824f0bf73fbc8fd7
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2023-04-05 19:22:18 +0700

    Initial Setup

---
+++ run_dev.bat
@@ -0,0 +1,1 @@
+nodemon -i src/js -e ts -x "tsc & python -m http.server -d src -b localhost 8080"
---
+++ run_dev.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sh
+nodemon -i src/js -e ts -x "tsc & python -m http.server -d src -b localhost 8080"
